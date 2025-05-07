c60798d26a1c46fe59074fcdb3c4173fb171505b
MoYuSOwO <hhj2006@outlook.com>
2025-05-08 00:19:19 +0800

    chore: debug

---
+++ src/main/java/io/github/MoYuSOwO/neoArtisan/item/ItemBehavior.java
@@ -0,0 +1,48 @@
+package io.github.MoYuSOwO.neoArtisan.item;
+
+import io.github.MoYuSOwO.neoArtisan.NeoArtisan;
+import net.minecraft.core.component.DataComponentMap;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.nbt.CompoundTag;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.inventory.CraftItemStack;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.components.FoodComponent;
+
+public final class ItemBehavior implements Listener {
+
+    private ItemBehavior() {}
+
+    static void init() {
+        NeoArtisan.registerListener(new ItemBehavior());
+    }
+
+    @EventHandler
+    private static void onAttackWithArtisanWeapon(EntityDamageByEntityEvent event) {
+        if (event.isCancelled()) return;
+        if (event.getDamager() instanceof Player player) {
+            Material material = player.getInventory().getItemInMainHand().getType();
+            System.out.println(material.getDefaultAttributeModifiers());
+
+            ItemStack itemStack = player.getInventory().getItemInMainHand();
+            ItemMeta itemMeta = itemStack.getItemMeta();
+            net.minecraft.world.item.ItemStack nmsItem = ((CraftItemStack) itemStack).handle;
+            DataComponentMap tag = nmsItem.getComponents();
+            var modifiers = tag.get(DataComponents.ATTRIBUTE_MODIFIERS);
+            if (modifiers != null) {
+                System.out.println(modifiers.modifiers());
+            }
+            System.out.println(itemMeta.getAttributeModifiers());
+            System.out.println(event.getDamage());
+        }
+    }
+
+}
