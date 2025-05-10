0fbdd2e29ea500679707c96b8cc351b49f97f3a9
MoYuSOwO <hhj2006@outlook.com>
2025-05-11 00:24:25 +0800

    feat: 有笨比忘记注册新API服务

--- src/main/java/io/github/moyusowo/neoartisan/block/ArtisanBlockInit.java
+++ src/main/java/io/github/moyusowo/neoartisan/block/ArtisanBlockInit.java
@@ -1,7 +1,13 @@
 package io.github.moyusowo.neoartisan.block;
 
+import io.github.moyusowo.neoartisan.NeoArtisan;
+import io.github.moyusowo.neoartisan.block.crop.ArtisanCropStorageImpl;
 import io.github.moyusowo.neoartisan.block.crop.CropRegistryImpl;
 import io.github.moyusowo.neoartisan.block.network.BlockMappingsManager;
+import io.github.moyusowo.neoartisanapi.api.block.crop.ArtisanCropStorage;
+import io.github.moyusowo.neoartisanapi.api.block.crop.CropRegistry;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.ServicePriority;
 
 public final class ArtisanBlockInit {
 
@@ -10,6 +16,18 @@ public final class ArtisanBlockInit {
     public static void init() {
         BlockMappingsManager.init();
         CropRegistryImpl.init();
+        Bukkit.getServicesManager().register(
+                CropRegistry.class,
+                CropRegistryImpl.getInstance(),
+                NeoArtisan.instance(),
+                ServicePriority.Normal
+        );
+        Bukkit.getServicesManager().register(
+                ArtisanCropStorage.class,
+                ArtisanCropStorageImpl.getInstance(),
+                NeoArtisan.instance(),
+                ServicePriority.Normal
+        );
     }
 
 }
